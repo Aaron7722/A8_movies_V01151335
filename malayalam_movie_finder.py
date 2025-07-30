a1a761c73c638661870c48067dcbb8631e5a0ebc
Aaron7722 <aaronthomas9035@gmail.com>
2025-07-30 20:34:11 +0530

    malayalam_movie_finder.py

--- malayalam_movie_finder.py
+++ malayalam_movie_finder.py
@@ -48,13 +48,6 @@ st.markdown("""
 
 😅 But let’s be honest: it’s often super hard to get good suggestions for hidden gems or evergreen class
 
-
-# Main page: intro text and image
-st.markdown("""
-🍿🎥 **Movies are a great deal in our day-to-day lives — *especially* for Keralites! 🇮🇳❤️**
-
-😅 But let’s be honest: it’s often super hard to get good suggestions for hidden gems or evergreen classics.
-
 ✨ So here’s a **fun little app** that suggests Malayalam movies based on your favorite genre! 🎞️👇
 
 ✅ **Pick a genre → get a list of movies with ratings & descriptions.**
